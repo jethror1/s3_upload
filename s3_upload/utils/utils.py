6cf28be4ecbeb464f8be782cacd9db94a51060cb
Jethro Rainford <rainford1995@gmail.com>
2024-09-30 16:11:47 +0100

    more boilerplate

---
+++ s3_upload/utils/utils.py
@@ -0,0 +1,32 @@
+def check_termination_files_exists(dir) -> bool:
+    """
+    _summary_
+
+    Parameters
+    ----------
+    dir : _type_
+        _description_
+
+    Returns
+    -------
+    bool
+        _description_
+    """
+    pass
+
+
+def check_is_sequencing_dir(dir) -> bool:
+    """
+    _summary_
+
+    Parameters
+    ----------
+    dir : _type_
+        _description_
+
+    Returns
+    -------
+    bool
+        _description_
+    """
+    pass
