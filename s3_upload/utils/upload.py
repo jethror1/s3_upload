89b7e74b991e01c8f4701986dffc35891284dbea
Jethro Rainford <rainford1995@gmail.com>
2024-10-03 12:55:31 +0100

    add functions for handling file upload

--- s3_upload/utils/upload.py
+++ s3_upload/utils/upload.py
@@ -6,8 +6,11 @@ from concurrent.futures import (
     wait,
     as_completed,
 )
+from os import path
 
 import boto3
+from botocore.config import Config
+from boto3.s3.transfer import TransferConfig
 
 
 def authenticate():
@@ -17,19 +20,45 @@ def authenticate():
     pass
 
 
-def upload_single_file(local_file):
+def upload_single_file(s3_client, bucket, remote_path, local_file):
     """
     Uploads single file into S3 storage bucket
 
     Parameters
     ----------
-    local_file : _type_
-        _description_
+    client : bootcore.client.S3
+        boto3 S3 client
+    bucket : str
+        S3 bucket to upload to
+    remote_path : str
+        parent directory in bucket to upload to
+    local_file : str
+        file and path to upload
+
+    Returns
+    -------
+    str
+        path and filename of uploaded file
+    str
+        ETag attribute of the uploaded file
     """
-    pass
+    # remove base directory
+    upload_file = local_file.lstrip(".").lstrip("/").replace("/genetics", "")
+    upload_file = path.join(remote_path, upload_file)
+
+    # set threshold for splitting across cores to 1GB and to not use
+    # multiple threads for single file upload to allow us to control
+    # this better from the config
+    config = TransferConfig(multipart_threshold=1024**3, use_threads=False)
+    s3_client.upload_file(local_file, bucket, upload_file, Config=config)
+
+    # ensure we can access the remote file to log the object ID
+    remote_object = s3_client.get_object(Bucket=bucket, Key=upload_file)
+
+    return local_file, remote_object.get("ETag")
 
 
-def single_core_threaded_upload(files, threads) -> list:
+def single_core_threaded_upload(files, bucket, remote_path, threads) -> list:
     """
     Uploads the given set of `files` to S3 on a single CPU core using
     maximum of n threads
@@ -38,15 +67,51 @@ def single_core_threaded_upload(files, threads) -> list:
     ----------
     files : list
         list of local files to upload
+    bucket : str
+        S3 bucket to upload to
+    remote_path : str
+        parent directory in bucket to upload to
     threads : int
         maximum number of threaded process to open per core
 
     Returns
     -------
-    list
-        _description_
+    dict
+        mapping of local file to ETag ID of uploaded file
     """
-    pass
+    # defining one S3 client per core due to boto3 clients being thread
+    # safe but not safe to share across processes due to expected response
+    # ordering: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/clients.html#caveats
+    session = boto3.session.Session()
+    s3_client = session.client(
+        "s3", config=Config(retries={"max_attempts": 10, "mode": "standard"})
+    )
+
+    uploaded_files = {}
+
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+        concurrent_jobs = {
+            executor.submit(
+                upload_single_file,
+                s3_client=s3_client,
+                bucket=bucket,
+                remote_path=remote_path,
+                local_file=item,
+            ): item
+            for item in files
+        }
+
+        for future in as_completed(concurrent_jobs):
+            # access returned output as each is returned in any order
+            try:
+                local_file, remote_id = future.result()
+                uploaded_files[local_file] = remote_id
+            except Exception as exc:
+                # catch any other errors that might get raised
+                print(f"\nError: {concurrent_jobs[future]}: {exc}")
+                raise exc
+
+    return uploaded_files
 
 
 def call_by_core(files, cores, threads) -> list:
@@ -65,7 +130,28 @@ def call_by_core(files, cores, threads) -> list:
 
     Returns
     -------
-    list
-        _description_
+    dict
+        mapping of local file to ETag ID of uploaded file
     """
-    pass
+    uploaded_files = {}
+
+    with ProcessPoolExecutor(max_workers=cores) as exe:
+        concurrent_jobs = {
+            exe.submit(
+                single_core_threaded_upload,
+                files=sub_files,
+                threads=threads,
+            ): sub_files
+            for sub_files in files
+        }
+
+        for future in as_completed(concurrent_jobs):
+            # access returned output as each is returned in any order
+            try:
+                uploaded_files = {**uploaded_files, **future.result()}
+            except Exception as exc:
+                # catch any other errors that might get raised
+                print(f"\nError: {concurrent_jobs[future]}: {exc}")
+                raise exc
+
+    return uploaded_files
