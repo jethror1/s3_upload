839288c9f97b2e25d0abe19bb903a5ce7b41629c
Jethro Rainford <rainford1995@gmail.com>
2024-10-10 12:45:41 +0100

    actually fix the params since AWS devs want to randomly wrap a function with captialised args and not have it match the docs because why not

--- s3_upload/utils/upload.py
+++ s3_upload/utils/upload.py
@@ -123,13 +123,11 @@ def upload_single_file(
     # this better from the config
     config = TransferConfig(multipart_threshold=1024**3, use_threads=False)
     s3_client.upload_file(
-        filename=local_file,
-        bucket=bucket,
-        key=upload_file,
+        Filename=local_file,
+        Bucket=bucket,
+        Key=upload_file,
         Config=config,
     )
-
-    # s3_client.upload_file(local_file, bucket, upload_file, Config=config)
 
     # ensure we can access the remote file to log the object ID
     remote_object = s3_client.get_object(Bucket=bucket, Key=upload_file)
