43b85aca8addb431dd763883478f0919dc5e4bba
Jethro Rainford <rainford1995@gmail.com>
2024-10-23 10:35:29 +0100

    update import path

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.path.append(
-    os.path.abspath(os.path.join(os.path.realpath(__file__), "../../"))
+    os.path.abspath(os.path.join(os.path.realpath(__file__), "../../../"))
 )
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "../test_data")
