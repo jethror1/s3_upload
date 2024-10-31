b3fd1a7513e893fb9d64473fe840204e730860dd
Jethro Rainford <rainford1995@gmail.com>
2024-10-31 15:02:28 +0000

    start e2e tests for 2 runs

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -64,7 +64,11 @@ def create_files(run_dir, *files):
         open(full_path, encoding="utf-8", mode="a").close()
 
 
-class TestE2ESingleSuccessfulRun(unittest.TestCase):
+class TestSingleCompleteRun(unittest.TestCase):
+    """
+    End to end tests for a single complete run in a monitored directory
+    being correctly uploaded into the specified bucket and remote path.
+    """
 
     @classmethod
     def setUpClass(cls):
@@ -261,3 +265,103 @@ class TestE2ESingleSuccessfulRun(unittest.TestCase):
             self.assertEqual(
                 self.mock_slack.call_args[1]["message"], expected_message
             )
+
+
+class TestTwoCompleteRunsInSeparateMonitorDirectories(unittest.TestCase):
+    """
+    End to end tests for uploading 2 completed sequencing runs that are
+    in separate monitored directories and being uploaded into separate
+    remote paths in the same specified bucket.
+
+    We will create locally `sequencer_a/run_1` and `sequencer_b/run_2`
+    which we then expect to upload to `s3_upload_e2e_test/{now}/
+    sequencer_a/run_1` and ``s3_upload_e2e_test/{now}/sequencer_b/run_2`
+    respectively.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        # create test sequencing runs in set monitored directories
+        cls.run_1 = os.path.join(TEST_DATA_DIR, "sequencer_a", "run_1")
+        cls.run_2 = os.path.join(TEST_DATA_DIR, "sequencer_b", "run_2")
+
+        # create as a complete runs with some example files
+        for run in (cls.run_1, cls.run_2):
+            create_files(
+                run,
+                "RunInfo.xml",
+                "CopyComplete.txt",
+                "Config/Options.cfg",
+                "InterOp/EventMetricsOut.bin",
+            )
+
+            shutil.copy(
+                os.path.join(TEST_DATA_DIR, "example_samplesheet.csv"),
+                os.path.join(run, "samplesheet.csv"),
+            )
+
+        # define full unique path to upload test runs to
+        now = datetime.now().strftime("%y%m%d_%H%M%S")
+        cls.remote_path = f"s3_upload_e2e_test/{now}/sequencer_a"
+
+        # add in the sequencer to monitor with test run
+        config_file = os.path.join(TEST_DATA_DIR, "test_config.json")
+        config = deepcopy(BASE_CONFIG)
+        config["log_dir"] = os.path.join(TEST_DATA_DIR, "logs")
+        config["monitor"].append(
+            {
+                "monitored_directories": [
+                    os.path.join(TEST_DATA_DIR, "sequencer_a"),
+                    os.path.join(TEST_DATA_DIR, "sequencer_b"),
+                ],
+                "bucket": S3_BUCKET,
+                "remote_path": cls.remote_path,
+            }
+        )
+
+        with open(config_file, "w") as fh:
+            json.dump(config, fh)
+
+        # mock command line args that would be set pointing to the config
+        cls.mock_args = patch("s3_upload.s3_upload.parse_args").start()
+        cls.mock_args.return_value = Namespace(
+            config=config_file,
+            dry_run=False,
+            mode="monitor",
+        )
+
+        # mock the file lock that stops concurrent uploads as this breaks
+        # when running unittest
+        cls.mock_flock = patch("s3_upload.s3_upload.acquire_lock").start()
+
+        cls.mock_slack = patch(
+            "s3_upload.s3_upload.slack.post_message"
+        ).start()
+
+        # call the main entry point to run the upload
+        s3_upload_main()
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.run_1)
+
+        os.remove(
+            os.path.join(TEST_DATA_DIR, "logs/uploads/run_1.upload.log.json")
+        )
+
+        os.remove(os.path.join(TEST_DATA_DIR, "test_config.json"))
+
+        # delete the logger log files
+        for log_file in glob(os.path.join(TEST_DATA_DIR, "logs", "*log*")):
+            os.remove(log_file)
+
+        # clean up the remote files we just uploaded
+        bucket = boto3.resource("s3").Bucket(S3_BUCKET)
+        objects = bucket.objects.filter(Prefix=cls.remote_path)
+        bucket.delete_objects(
+            Delete={"Objects": [{"Key": obj.key} for obj in objects]}
+        )
+
+        cls.mock_args.stop()
+        cls.mock_flock.stop()
+        cls.mock_slack.stop()
