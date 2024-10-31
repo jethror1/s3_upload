6a38154d02c27e39eb75ef9e759dd19d61ded28d
Jethro Rainford <rainford1995@gmail.com>
2024-10-31 10:26:34 +0000

    add more e2e tests

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -3,20 +3,15 @@ from copy import deepcopy
 from datetime import datetime
 from glob import glob
 import json
+import unittest
 from unittest.mock import patch
 import os
 import shutil
-import sys
-from uuid import uuid4
-import unittest
-from unittest.mock import Mock, patch
 
 import boto3
-import pytest
 
 from e2e import TEST_DATA_DIR
 from s3_upload.s3_upload import main as s3_upload_main
-from s3_upload.utils.upload import as_completed
 
 
 BASE_CONFIG = {
@@ -24,8 +19,8 @@ BASE_CONFIG = {
     "max_threads": 8,
     "log_level": "DEBUG",
     "log_dir": "",
-    "slack_log_webhook": "log_webhook",
-    "slack_alert_webhook": "alert_webhook",
+    "slack_log_webhook": "",
+    "slack_alert_webhook": "",
     "monitor": [
         {
             "monitored_directories": [],
@@ -75,6 +70,7 @@ class TestE2ESingleSuccessfulRun(unittest.TestCase):
 
         cls.mock_flock = patch("s3_upload.s3_upload.acquire_lock").start()
 
+        # call the main entry point to run the upload
         s3_upload_main()
 
     @classmethod
@@ -85,9 +81,9 @@ class TestE2ESingleSuccessfulRun(unittest.TestCase):
             os.path.join(TEST_DATA_DIR, "logs/uploads/run_1.upload.log.json")
         )
 
-        # # delete the logger log files
-        # for log_file in glob(os.path.join(TEST_DATA_DIR, "logs", "*log*")):
-        #     os.remove(log_file)
+        # delete the logger log files
+        for log_file in glob(os.path.join(TEST_DATA_DIR, "logs", "*log*")):
+            os.remove(log_file)
 
         # clean up the remote files we just uploaded
         bucket = boto3.resource("s3").Bucket(cls.bucket)
@@ -116,33 +112,79 @@ class TestE2ESingleSuccessfulRun(unittest.TestCase):
             "CopyComplete.txt",
             "run_1_samplesheet.csv",
         ]
-        expected_remote_files = [
-            os.path.join(self.remote_path, f) for f in local_files
-        ]
+        expected_remote_files = sorted(
+            [os.path.join(self.remote_path, "run_1", f) for f in local_files]
+        )
 
         bucket = boto3.resource("s3").Bucket(self.bucket)
-        objects = bucket.objects.filter(Prefix=self.remote_path)
-        print(self.remote_path)
-        print(objects)
-        print([o for o in objects])
-        # exit(1).start()
+        uploaded_objects = bucket.objects.filter(Prefix=self.remote_path)
+        uploaded_files = sorted([x.key for x in uploaded_objects])
+
+        self.assertEqual(uploaded_files, expected_remote_files)
 
     def test_upload_log_file_for_fully_uploaded_run_correct(self):
         """
         Test that the log file that details what has been uploaded and
-        marks a run as complete is correct
-        """
-        pass
+        marks a run as complete is correct.
 
-    def test_uploaded_run_not_picked_up_to_upload_again(self):
+        The uploaded files contains ETag IDs return from S3, so we will
+        check separately just for the logged uploaded files
+        """
+        expected_top_level_log_contents = {
+            "run_id": "run_1",
+            "run path": "/home/jethro/Projects/s3_upload/tests/e2e/test_data/sequencer_a/run_1",
+            "completed": True,
+            "total_local_files": 3,
+            "total_uploaded_files": 3,
+            "total_failed_upload": 0,
+            "failed_upload_files": [],
+        }
+        expected_uploaded_files = [
+            "/home/jethro/Projects/s3_upload/tests/e2e/test_data/sequencer_a/run_1/CopyComplete.txt",
+            "/home/jethro/Projects/s3_upload/tests/e2e/test_data/sequencer_a/run_1/RunInfo.xml",
+            "/home/jethro/Projects/s3_upload/tests/e2e/test_data/sequencer_a/run_1/run_1_samplesheet.csv",
+        ]
+
+        upload_log = os.path.join(
+            TEST_DATA_DIR, "logs/uploads/run_1.upload.log.json"
+        )
+
+        with open(upload_log, "r") as fh:
+            log_contents = json.load(fh)
+
+        with self.subTest("correct top level of log"):
+            self.assertDictContainsSubset(
+                expected_top_level_log_contents, log_contents
+            )
+
+        with self.subTest("correct local files uploaded in log"):
+            uploaded_files = sorted(log_contents["uploaded_files"].keys())
+
+            self.assertEqual(expected_uploaded_files, uploaded_files)
+
+    @patch("s3_upload.s3_upload.sys.exit")
+    def test_uploaded_run_not_picked_up_to_upload_again(self, mock_exit):
         """
         Test that when monitor runs again that the uploaded run does not
         get triggered to upload again
         """
+        with self.assertLogs("s3_upload") as log:
+            s3_upload_main()
 
-    def test_when_nothing_to_upload_that_exit_is_clean(self):
+            self.assertIn(
+                "No sequencing runs requiring upload found. Exiting now.",
+                "".join(log.output),
+            )
+
+    @patch("s3_upload.s3_upload.sys.exit")
+    def test_when_nothing_to_upload_that_exit_code_is_zero(self, mock_exit):
         """
         Test that when there is nothing to upload that we cleanly exit
-        pass
         """
-        pass
+        s3_upload_main()
+
+        with self.subTest("exit called"):
+            self.assertEqual(mock_exit.call_count, 1)
+
+        with self.subTest("exit code"):
+            self.assertEqual(mock_exit.call_args[0][0], 0)
