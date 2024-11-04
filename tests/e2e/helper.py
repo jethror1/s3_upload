946479c4c71b29eeea8ed5c4bb921c11ccda605a
Jethro Rainford <rainford1995@gmail.com>
2024-11-04 11:19:36 +0000

    add helper functions to read log files in

--- tests/e2e/helper.py
+++ tests/e2e/helper.py
@@ -1,5 +1,8 @@
+import json
 from os import makedirs, path
 from pathlib import Path
+
+from e2e import TEST_DATA_DIR
 
 
 def create_files(run_dir, *files):
@@ -21,3 +24,37 @@ def create_files(run_dir, *files):
 
         makedirs(parent_dir, exist_ok=True)
         open(full_path, encoding="utf-8", mode="a").close()
+
+
+def read_upload_log() -> dict:
+    """
+    Read in the run upload log file
+
+    Returns
+    -------
+    dict
+        contents of run upload log file
+    """
+    with open(
+        path.join(TEST_DATA_DIR, "logs/uploads/run_1.upload.log.json"),
+        encoding="utf8",
+        mode="r",
+    ) as fh:
+        return json.load(fh)
+
+
+def read_stdout_stderr_log() -> list:
+    """
+    Read the stdout / stderr log file s3_upload.log
+
+    Returns
+    -------
+    list
+        contents of log file
+    """
+    with open(
+        path.join(TEST_DATA_DIR, "logs/s3_upload.log"),
+        encoding="utf8",
+        mode="r",
+    ) as fh:
+        return fh.read().splitlines()
