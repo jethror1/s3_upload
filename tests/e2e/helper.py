3b4db5d76c1a6ebbb98883b35ea82cf1bfd1efe6
Jethro Rainford <rainford1995@gmail.com>
2024-11-04 11:52:54 +0000

    add test cleanup helper functions

--- tests/e2e/helper.py
+++ tests/e2e/helper.py
@@ -1,8 +1,16 @@
-import json
-from os import makedirs, path
-from pathlib import Path
+"""
+Simple helper functions used in most tests for set up and clean up
+"""
 
-from e2e import TEST_DATA_DIR
+from glob import glob
+import json
+from os import makedirs, path, remove
+from pathlib import Path
+import shutil
+
+import boto3
+
+from e2e import S3_BUCKET, TEST_DATA_DIR
 
 
 def create_files(run_dir, *files):
@@ -24,6 +32,40 @@ def create_files(run_dir, *files):
 
         makedirs(parent_dir, exist_ok=True)
         open(full_path, encoding="utf-8", mode="a").close()
+
+
+def cleanup_local_test_files(*run_dirs) -> None:
+    """
+    Clean up the test files and logs etc.
+
+    To be called from TearDownClass to revert the test set up state.
+    """
+    for run_dir in run_dirs:
+        shutil.rmtree(Path(run_dir).parent)
+
+    for log_file in glob(path.join(TEST_DATA_DIR, "logs", "*log*")):
+        remove(log_file)
+
+    remove(path.join(TEST_DATA_DIR, "logs/uploads/run_1.upload.log.json"))
+    remove(path.join(TEST_DATA_DIR, "test_config.json"))
+
+
+def cleanup_remote_files(remote_path) -> None:
+    """
+    Clean up the uploaded test files from the remote path in the test
+    S3 bucket
+
+    Parameters
+    ----------
+    remote_path : str
+        path where files were uploaded to
+    """
+    bucket = boto3.resource("s3").Bucket(S3_BUCKET)
+    objects = bucket.objects.filter(Prefix=remote_path)
+    objects = [{"Key": obj.key} for obj in objects]
+
+    if objects:
+        bucket.delete_objects(Delete={"Objects": objects})
 
 
 def read_upload_log() -> dict:
